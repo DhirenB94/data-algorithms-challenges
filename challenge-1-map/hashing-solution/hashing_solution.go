9980b96dcf5da360bbf92fb0e8dc4404a7107244
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-03-07 21:31:18 +0000

    hashing solution : has and get methods

--- challenge-1-map/hashing-solution/hashing_solution.go
+++ challenge-1-map/hashing-solution/hashing_solution.go
@@ -18,10 +18,30 @@ func hash(key int) int {
 
 func (h *HashMap) Has(key int) bool {
 	indexPosition := hash(key)
-
-	if h.Buckets[indexPosition] == nil {
-		fmt.Printf("element with key: %v not present\n", key)
-		return false
+	if h.Buckets[indexPosition] != nil {
+		//iterate through all elements in the list at that index position
+		firstNodeInList := h.Buckets[indexPosition]
+		for n := firstNodeInList; n != nil; n = n.Next {
+			if n.Key == key {
+				return true
+			}
+		}
 	}
-	return true
+	fmt.Printf("element with key: %v not present\n", key)
+	return false
+}
+
+func (h HashMap) Get(key int) (string, bool) {
+	indexPosition := hash(key)
+	if h.Buckets[indexPosition] != nil {
+		//check if it is in the linked list of the index position
+		firstNodeInList := h.Buckets[indexPosition]
+		for n := firstNodeInList; n != nil; n = n.Next {
+			if n.Key == key {
+				return n.Value, true
+			}
+		}
+	}
+	fmt.Printf("element with key: %v not present\n", key)
+	return "", false
 }
--- challenge-1-map/hashing-solution/hashing_solution_test.go
+++ challenge-1-map/hashing-solution/hashing_solution_test.go
@@ -11,13 +11,22 @@ func TestHashKeys(t *testing.T) {
 		t.Run("return true if key is present", func(t *testing.T) {
 			hashTable := newHashMap(50)
 			newNode := &hashing_solution.Node{
-				Key:   5,
-				Value: "five",
+				Key:   45,
+				Value: "foo",
 			}
-			hashTable.Buckets[5] = newNode
+			nextNode := &hashing_solution.Node{
+				Key:   95,
+				Value: "bar",
+			}
 
-			answer := hashTable.Has(5)
+			hashTable.Buckets[45] = newNode
+			newNode.Next = nextNode
+
+			answer := hashTable.Has(45)
 			assert.Equal(t, true, answer)
+
+			answer2 := hashTable.Has(95)
+			assert.Equal(t, true, answer2)
 		})
 		t.Run("return false if key is not present", func(t *testing.T) {
 			hashTable := newHashMap(50)
@@ -28,10 +37,31 @@ func TestHashKeys(t *testing.T) {
 	})
 	t.Run("Get", func(t *testing.T) {
 		t.Run("gets the value if key present", func(t *testing.T) {
+			hashTable := newHashMap(50)
+			newNode := &hashing_solution.Node{
+				Key:   45,
+				Value: "foo",
+			}
+			nextNode := &hashing_solution.Node{
+				Key:   95,
+				Value: "bar",
+			}
 
+			hashTable.Buckets[45] = newNode
+			newNode.Next = nextNode
+
+			answer, _ := hashTable.Get(45)
+			assert.Equal(t, "foo", answer)
+
+			answer2, _ := hashTable.Get(95)
+			assert.Equal(t, "bar", answer2)
 		})
-		t.Run("returns empty string if key not present", func(t *testing.T) {
+		t.Run("returns empty string and false if key not present", func(t *testing.T) {
+			hashTable := newHashMap(50)
 
+			answer, isPresent := hashTable.Get(45)
+			assert.Equal(t, "", answer)
+			assert.False(t, isPresent)
 		})
 	})
 }
