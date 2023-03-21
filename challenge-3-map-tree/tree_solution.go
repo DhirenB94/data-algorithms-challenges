0818169d717bb961f9dfe582938f170e8a2630c6
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-03-21 20:42:17 +0000

    tree solution : correct New() to return the interface and stop exporting

--- challenge-3-map-tree/tree_solution.go
+++ challenge-3-map-tree/tree_solution.go
@@ -1,140 +1,142 @@
 package tree
 
-type TreeNode struct {
-	Key       int
-	Value     string
-	LeftNode  *TreeNode
-	RightNode *TreeNode
+import interfaces "data"
+
+type treeNode struct {
+	key       int
+	value     string
+	leftNode  *treeNode
+	rightNode *treeNode
 }
 
-type BinarySearchTree struct {
-	Root *TreeNode
+type binarySearchTree struct {
+	root *treeNode
 }
 
-func NewBinarySearchTree(treeNode *TreeNode) *BinarySearchTree {
-	return &BinarySearchTree{Root: treeNode}
+func NewBinarySearchTree() interfaces.Operations {
+	return &binarySearchTree{root: &treeNode{}}
 }
 
-func (bst *BinarySearchTree) Has(key int) bool {
-	_, isPresent := searchNode(bst.Root, key)
+func (bst *binarySearchTree) Has(key int) bool {
+	_, isPresent := searchNode(bst.root, key)
 	return isPresent
 }
 
-func (bst *BinarySearchTree) Get(key int) (string, bool) {
-	return searchNode(bst.Root, key)
+func (bst *binarySearchTree) Get(key int) (string, bool) {
+	return searchNode(bst.root, key)
 }
 
-func (bst *BinarySearchTree) Set(key int, value string) (string, bool) {
-	newNode := &TreeNode{
-		Key:   key,
-		Value: value,
+func (bst *binarySearchTree) Set(key int, value string) (string, bool) {
+	newNode := &treeNode{
+		key:   key,
+		value: value,
 	}
-	if bst.Root == nil {
-		bst.Root = newNode
+	if bst.root == nil {
+		bst.root = newNode
 		return "", true
 	}
-	return insertNode(bst.Root, key, value)
+	return insertNode(bst.root, key, value)
 }
 
-func (bst *BinarySearchTree) Remove(key int) (string, bool) {
+func (bst *binarySearchTree) Remove(key int) (string, bool) {
 	getValue, isGot := bst.Get(key)
 	if !isGot {
 		return "", false
 	}
-	removeNode(bst.Root, key)
+	removeNode(bst.root, key)
 	return getValue, true
 }
 
-func searchNode(treeNode *TreeNode, key int) (string, bool) {
+func searchNode(treeNode *treeNode, key int) (string, bool) {
 	//in the case of an empty BST or when you reach a leaf node
 	if treeNode == nil {
 		return "", false
 	}
-	if key == treeNode.Key {
-		return treeNode.Value, true
+	if key == treeNode.key {
+		return treeNode.value, true
 	}
 	//if key is smaller than the key of the current node, keep searching to the left
-	if key < treeNode.Key {
-		return searchNode(treeNode.LeftNode, key)
+	if key < treeNode.key {
+		return searchNode(treeNode.leftNode, key)
 	}
 	//if key is greater than the key of the current node, keep searching to the Right
-	if key > treeNode.Key {
-		return searchNode(treeNode.RightNode, key)
+	if key > treeNode.key {
+		return searchNode(treeNode.rightNode, key)
 	}
 	return "", false
 }
 
-func insertNode(node *TreeNode, key int, value string) (string, bool) {
-	newNode := &TreeNode{
-		Key:   key,
-		Value: value,
+func insertNode(node *treeNode, key int, value string) (string, bool) {
+	newNode := &treeNode{
+		key:   key,
+		value: value,
 	}
 
 	//If the key is present update the value and return the old value
-	if key == node.Key {
-		oldValue := node.Value
-		node.Value = value
+	if key == node.key {
+		oldValue := node.value
+		node.value = value
 		return oldValue, true
 	}
-	if key < node.Key {
-		if node.LeftNode == nil {
-			node.LeftNode = newNode
+	if key < node.key {
+		if node.leftNode == nil {
+			node.leftNode = newNode
 			return "", true
 		}
-		return insertNode(node.LeftNode, key, value)
+		return insertNode(node.leftNode, key, value)
 	}
-	if key > node.Key {
-		if node.RightNode == nil {
-			node.RightNode = newNode
+	if key > node.key {
+		if node.rightNode == nil {
+			node.rightNode = newNode
 			return "", true
 		}
-		return insertNode(node.RightNode, key, value)
+		return insertNode(node.rightNode, key, value)
 	}
 	return "", false
 }
 
-func removeNode(node *TreeNode, key int) *TreeNode {
+func removeNode(node *treeNode, key int) *treeNode {
 	if node == nil {
 		return nil
 	}
 	//Finding the key
-	if key < node.Key {
-		node.LeftNode = removeNode(node.LeftNode, key)
+	if key < node.key {
+		node.leftNode = removeNode(node.leftNode, key)
 	}
-	if key > node.Key {
-		node.RightNode = removeNode(node.RightNode, key)
+	if key > node.key {
+		node.rightNode = removeNode(node.rightNode, key)
 	}
-	if key == node.Key {
+	if key == node.key {
 		//if the node to be removed is a leaf node
-		if node.LeftNode == nil && node.RightNode == nil {
+		if node.leftNode == nil && node.rightNode == nil {
 			node = nil
 			return nil
 		}
 		//if the node to be removed has a L/R subtree only
-		if node.LeftNode == nil {
-			node = node.RightNode
+		if node.leftNode == nil {
+			node = node.rightNode
 		}
-		if node.RightNode == nil {
-			node = node.LeftNode
+		if node.rightNode == nil {
+			node = node.leftNode
 		}
 		//if the node to be removed has BOTH subtrees
-		if node.LeftNode != nil && node.RightNode != nil {
+		if node.leftNode != nil && node.rightNode != nil {
 			//chose the R subtree and go as far left as possible to find the smallest node
-			smallestNode := smallestNode(node.RightNode)
+			smallestNode := smallestNode(node.rightNode)
 			//copy the smallest node found to the node to be removed
-			node.Key = smallestNode.Key
-			node.Value = smallestNode.Value
+			node.key = smallestNode.key
+			node.value = smallestNode.value
 			//the smallest node will always either be a leaf node or have a max of 1 child (in this case 1 right child only)
 			//call the remove method on it, but start from the right node of the original (as youve copied smallest node to the original, it would re-enter the loop)
-			node.RightNode = removeNode(node.RightNode, smallestNode.Key)
+			node.rightNode = removeNode(node.rightNode, smallestNode.key)
 		}
 	}
 	return node
 }
 
-func smallestNode(node *TreeNode) *TreeNode {
-	for node.LeftNode != nil {
-		node = node.LeftNode
+func smallestNode(node *treeNode) *treeNode {
+	for node.leftNode != nil {
+		node = node.leftNode
 	}
 	return node
 }
--- challenge-3-map-tree/tree_solution_test.go
+++ challenge-3-map-tree/tree_solution_test.go
@@ -9,169 +9,129 @@ import (
 func TestBinarySearchTree(t *testing.T) {
 	t.Run("Has", func(t *testing.T) {
 		t.Run("return true when the element is found and false when it is not", func(t *testing.T) {
-			rootNode := &tree.TreeNode{
-				Key:   50,
-				Value: "abc",
-			}
-
-			newBst := tree.NewBinarySearchTree(rootNode)
+			newBst := tree.NewBinarySearchTree()
 
 			newBst.Set(100, "def")
 			newBst.Set(75, "ghi")
 			newBst.Set(25, "jkl")
 			newBst.Set(1, "mno")
 
-			shouldContain := newBst.Has(75)
-			assert.True(t, shouldContain)
-			shouldNotContain := newBst.Has(99)
-			assert.False(t, shouldNotContain)
+			has := newBst.Has(75)
+			assert.True(t, has)
+			has = newBst.Has(99)
+			assert.False(t, has)
 		})
 	})
 	t.Run("Get", func(t *testing.T) {
 		t.Run("return the value and true when the element is found", func(t *testing.T) {
-			rootNode := &tree.TreeNode{
-				Key:   50,
-				Value: "abc",
-			}
-
-			newBst := tree.NewBinarySearchTree(rootNode)
+			newBst := tree.NewBinarySearchTree()
+			expectedValue := "ghi"
 
 			newBst.Set(100, "def")
+			newBst.Set(75, "ghi")
 			newBst.Set(25, "jkl")
-			newBst.Set(10, "")
+			newBst.Set(1, "mno")
 
-			value, isPresent := newBst.Get(25)
-			assert.Equal(t, "jkl", value)
-			assert.True(t, isPresent)
-
-			value2, isPresent2 := newBst.Get(10)
-			assert.Empty(t, value2)
-			assert.True(t, isPresent2)
-
+			getValue, isGot := newBst.Get(75)
+			assert.Equal(t, expectedValue, getValue)
+			assert.True(t, isGot)
 		})
 		t.Run("return empty string and false when value is not found", func(t *testing.T) {
-			rootNode := &tree.TreeNode{
-				Key:   50,
-				Value: "abc",
-			}
-
-			newBst := tree.NewBinarySearchTree(rootNode)
+			newBst := tree.NewBinarySearchTree()
+			expectedValue := ""
 
 			newBst.Set(100, "def")
+			newBst.Set(75, "ghi")
 			newBst.Set(25, "jkl")
-			newBst.Set(10, "")
+			newBst.Set(1, "mno")
 
-			value, isPresent := newBst.Get(35)
-			assert.Empty(t, value)
-			assert.False(t, isPresent)
+			getValue, isGot := newBst.Get(35)
+			assert.Equal(t, expectedValue, getValue)
+			assert.False(t, isGot)
 		})
 	})
 	t.Run("Set", func(t *testing.T) {
-		t.Run("insert a node if the key is not present", func(t *testing.T) {
-			rootNode := &tree.TreeNode{
-				Key:   50,
-				Value: "abc",
-			}
-			newBst := tree.NewBinarySearchTree(rootNode)
+		t.Run("return an empty string and true when node is not present", func(t *testing.T) {
+			newBst := tree.NewBinarySearchTree()
+			expectedValue := "def"
+			expecteSetValue := ""
 
-			value, isSet := newBst.Set(105, "ddd")
-			assert.Empty(t, value)
+			setValue, isSet := newBst.Set(100, "def")
+			assert.Equal(t, expecteSetValue, setValue)
 			assert.True(t, isSet)
 
-			getValue, isPresent := newBst.Get(105)
-			assert.Equal(t, "ddd", getValue)
-			assert.True(t, isPresent)
+			getValue, isGot := newBst.Get(100)
+			assert.Equal(t, expectedValue, getValue)
+			assert.True(t, isGot)
 		})
-		t.Run("if the key is present, insert new value and return old value", func(t *testing.T) {
-			rootNode := &tree.TreeNode{
-				Key:   50,
-				Value: "abc",
-			}
-			newBst := tree.NewBinarySearchTree(rootNode)
+		t.Run("if the key is present, insert new value and return old value and true", func(t *testing.T) {
+			newBst := tree.NewBinarySearchTree()
+			expectedOldValue := "def"
+			expectedGetValue := "ghi"
 
-			setValue, _ := newBst.Set(105, "originalValue")
-			assert.Empty(t, setValue)
-
-			oldValue, isSet2 := newBst.Set(105, "newValue")
-			assert.Equal(t, "originalValue", oldValue)
-			assert.True(t, isSet2)
-
-			getValue, isPresent := newBst.Get(105)
-			assert.Equal(t, "newValue", getValue)
-			assert.True(t, isPresent)
-		})
-		t.Run("able to insert a node if root node is nil", func(t *testing.T) {
-			newBst := tree.NewBinarySearchTree(nil)
-			answer, isSet := newBst.Set(200, "hhh")
-			assert.Empty(t, answer)
+			newBst.Set(100, "def")
+			oldValue, isSet := newBst.Set(100, "ghi")
+			assert.Equal(t, expectedOldValue, oldValue)
 			assert.True(t, isSet)
 
-			value, got := newBst.Get(200)
-			assert.Equal(t, "hhh", value)
-			assert.True(t, got)
+			getValue, isGot := newBst.Get(100)
+			assert.Equal(t, expectedGetValue, getValue)
+			assert.True(t, isGot)
 		})
-
 	})
 	t.Run("Remove", func(t *testing.T) {
 		t.Run("return empty string and false if node to be removed not present", func(t *testing.T) {
-			rootNode := &tree.TreeNode{
-				Key:   100,
-				Value: "100",
-			}
-			newBst := tree.NewBinarySearchTree(rootNode)
-			newBst.Set(75, "75")
-			newBst.Set(113, "113")
+			newBst := tree.NewBinarySearchTree()
 
-			removed, wasRemoved := newBst.Remove(20)
-			assert.Empty(t, removed)
-			assert.False(t, wasRemoved)
+			newBst.Set(100, "def")
+			newBst.Set(75, "ghi")
+			newBst.Set(25, "jkl")
+			newBst.Set(1, "mno")
+
+			removedValue, isRemoved := newBst.Remove(21)
+			assert.Empty(t, removedValue)
+			assert.False(t, isRemoved)
 		})
 		t.Run("removes the element with the given key", func(t *testing.T) {
 			t.Run("when node to be removed is a leaf node", func(t *testing.T) {
-				rootNode := &tree.TreeNode{
-					Key:   100,
-					Value: "100",
-				}
-				newBst := tree.NewBinarySearchTree(rootNode)
+				newBst := tree.NewBinarySearchTree()
+				expectedRemovedValue := "75"
+
+				newBst.Set(100, "100")
 				newBst.Set(75, "75")
 				newBst.Set(113, "113")
 
-				removed, wasRemoved := newBst.Remove(75)
-				assert.Equal(t, "75", removed)
-				assert.True(t, wasRemoved)
+				removedValue, isRemoved := newBst.Remove(75)
+				assert.Equal(t, expectedRemovedValue, removedValue)
+				assert.True(t, isRemoved)
 
-				getValue, isGot := newBst.Get(75)
-				assert.Empty(t, getValue)
-				assert.False(t, isGot)
+				has := newBst.Has(75)
+				assert.False(t, has)
 			})
 			t.Run("when node to be removed has one child node", func(t *testing.T) {
-				rootNode := &tree.TreeNode{
-					Key:   100,
-					Value: "100",
-				}
-				newBst := tree.NewBinarySearchTree(rootNode)
+				newBst := tree.NewBinarySearchTree()
+				expectedRemovedValue := "75"
+
+				newBst.Set(100, "100")
 				newBst.Set(75, "75")
 				newBst.Set(66, "66")
 				newBst.Set(113, "113")
 
-				removed, wasRemoved := newBst.Remove(75)
-				assert.Equal(t, "75", removed)
-				assert.True(t, wasRemoved)
+				removedValue, isRemoved := newBst.Remove(75)
+				assert.Equal(t, expectedRemovedValue, removedValue)
+				assert.True(t, isRemoved)
 
-				getValue, isGot := newBst.Get(75)
-				assert.Empty(t, getValue)
-				assert.False(t, isGot)
+				has := newBst.Has(75)
+				assert.False(t, has)
 
-				assert.Equal(t, 66, rootNode.LeftNode.Key)
-				assert.Nil(t, rootNode.LeftNode.LeftNode)
-
+				has = newBst.Has(66)
+				assert.True(t, has)
 			})
 			t.Run("when node to be removed has left and right child nodes", func(t *testing.T) {
-				rootNode := &tree.TreeNode{
-					Key:   100,
-					Value: "100",
-				}
-				newBst := tree.NewBinarySearchTree(rootNode)
+				newBst := tree.NewBinarySearchTree()
+				expectedRemovedValue := "90"
+
+				newBst.Set(100, "100")
 				newBst.Set(75, "75")
 				newBst.Set(150, "150")
 				newBst.Set(60, "60")
@@ -180,19 +140,24 @@ func TestBinarySearchTree(t *testing.T) {
 				newBst.Set(95, "95")
 				newBst.Set(92, "92")
 
-				removed, wasRemoved := newBst.Remove(90)
-				assert.Equal(t, "90", removed)
-				assert.True(t, wasRemoved)
+				removedValue, isremoved := newBst.Remove(90)
+				assert.Equal(t, expectedRemovedValue, removedValue)
+				assert.True(t, isremoved)
 
 				getValue, isGot := newBst.Get(90)
 				assert.Empty(t, getValue)
 				assert.False(t, isGot)
 
-				assert.Equal(t, 92, rootNode.LeftNode.RightNode.Key)
-
 				newBst.Remove(100)
-				assert.Equal(t, 150, rootNode.Key)
-				assert.Empty(t, rootNode.RightNode)
+
+				has := newBst.Has(85)
+				assert.True(t, has)
+
+				has = newBst.Has(92)
+				assert.True(t, has)
+
+				has = newBst.Has(150)
+				assert.True(t, has)
 			})
 		})
 	})
