e0b359718ed4c160eb6e10a72840a45ca1dab89f
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-03-21 10:41:23 +0000

    simple solution : stop exporting the types

--- challenge-1-map-simple/simple-slice-solution/simple.go
+++ challenge-1-map-simple/simple-slice-solution/simple.go
@@ -11,20 +11,20 @@ type Operations interface {
 	Set(key int, value string) string
 }
 
-type SimpleKeyValue struct {
-	Key   int
-	Value string
+type simpleKeyValue struct {
+	key   int
+	value string
 }
 
-type SliceKeyValues struct {
-	KeyValues []SimpleKeyValue
+type sliceKeyValues struct {
+	keyValues []simpleKeyValue
 }
 
 func NewSliceKeyValues() Operations {
-	return &SliceKeyValues{make([]SimpleKeyValue, 0)}
+	return &sliceKeyValues{}
 }
 
-func (s *SliceKeyValues) Has(key int) bool {
+func (s *sliceKeyValues) Has(key int) bool {
 	if s.indexOf(key) == -1 {
 		fmt.Printf("element with key: %v not present\n", key)
 		return false
@@ -32,46 +32,46 @@ func (s *SliceKeyValues) Has(key int) bool {
 	return true
 }
 
-func (s *SliceKeyValues) Get(key int) (string, bool) {
-	for _, k := range s.KeyValues {
-		if k.Key == key {
-			return k.Value, true
+func (s *sliceKeyValues) Get(key int) (string, bool) {
+	for _, k := range s.keyValues {
+		if k.key == key {
+			return k.value, true
 		}
 	}
 	fmt.Printf("element with key: %v not present\n", key)
 	return "", false
 }
 
-func (s *SliceKeyValues) Remove(key int) (string, bool) {
+func (s *sliceKeyValues) Remove(key int) (string, bool) {
 	keyIndex := s.indexOf(key)
 	if keyIndex == -1 {
 		fmt.Printf("Key: %v, not found\n", key)
 		return "", false
 	}
 	value, isPresent := s.Get(key)
-	s.KeyValues = append(s.KeyValues[:keyIndex], s.KeyValues[keyIndex+1:]...)
+	s.keyValues = append(s.keyValues[:keyIndex], s.keyValues[keyIndex+1:]...)
 
 	fmt.Printf("removed element with key: %v and value: :%v\n", key, value)
 	return value, isPresent
 }
 
-func (s *SliceKeyValues) Set(key int, value string) string {
+func (s *sliceKeyValues) Set(key int, value string) string {
 	keyIndex := s.indexOf(key)
 	if keyIndex == -1 {
-		s.KeyValues = append(s.KeyValues, SimpleKeyValue{
-			Key:   key,
-			Value: value,
+		s.keyValues = append(s.keyValues, simpleKeyValue{
+			key:   key,
+			value: value,
 		})
 		return ""
 	}
 	oldValue, _ := s.Get(key)
-	s.KeyValues[keyIndex].Value = value
+	s.keyValues[keyIndex].value = value
 	return oldValue
 }
 
-func (s *SliceKeyValues) indexOf(key int) int {
-	for i, k := range s.KeyValues {
-		if k.Key == key {
+func (s *sliceKeyValues) indexOf(key int) int {
+	for i, k := range s.keyValues {
+		if k.key == key {
 			return i
 		}
 	}
