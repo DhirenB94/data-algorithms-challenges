eba41d361a093526f6e548ddc2966a58dcb363b2
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-04-07 15:20:55 +0100

    generics : define a custom type and use it as the type constraint in the interface, update the simple solution

--- challenge-1-map-simple/simple-slice-solution/simple.go
+++ challenge-1-map-simple/simple-slice-solution/simple.go
@@ -5,27 +5,27 @@ import (
 	"fmt"
 )
 
-type simpleKeyValue[V any] struct {
-	key   int
+type simpleKeyValue[K, V interfaces.CustomType] struct {
+	key   K
 	value V
 }
 
-type sliceKeyValues[V any] struct {
-	keyValues []simpleKeyValue[V]
+type sliceKeyValues[K, V interfaces.CustomType] struct {
+	keyValues []simpleKeyValue[K, V]
 }
 
-func NewSliceKeyValues[V any]() interfaces.Operations[V] {
-	return &sliceKeyValues[V]{}
+func NewSliceKeyValues[K, V interfaces.CustomType]() interfaces.Operations[K, V] {
+	return &sliceKeyValues[K, V]{}
 }
 
-func (s *sliceKeyValues[V]) Has(key int) bool {
+func (s *sliceKeyValues[K, V]) Has(key K) bool {
 	if s.indexOf(key) == -1 {
 		return false
 	}
 	return true
 }
 
-func (s *sliceKeyValues[V]) Get(key int) *V {
+func (s *sliceKeyValues[K, V]) Get(key K) *V {
 	for _, k := range s.keyValues {
 		if k.key == key {
 			return &k.value
@@ -34,7 +34,7 @@ func (s *sliceKeyValues[V]) Get(key int) *V {
 	return nil
 }
 
-func (s *sliceKeyValues[V]) Remove(key int) *V {
+func (s *sliceKeyValues[K, V]) Remove(key K) *V {
 	keyIndex := s.indexOf(key)
 	if keyIndex == -1 {
 		return nil
@@ -45,10 +45,10 @@ func (s *sliceKeyValues[V]) Remove(key int) *V {
 	return value
 }
 
-func (s *sliceKeyValues[V]) Set(key int, value V) *V {
+func (s *sliceKeyValues[K, V]) Set(key K, value V) *V {
 	keyIndex := s.indexOf(key)
 	if keyIndex == -1 {
-		s.keyValues = append(s.keyValues, simpleKeyValue[V]{
+		s.keyValues = append(s.keyValues, simpleKeyValue[K, V]{
 			key:   key,
 			value: value,
 		})
@@ -59,7 +59,7 @@ func (s *sliceKeyValues[V]) Set(key int, value V) *V {
 	return oldValue
 }
 
-func (s *sliceKeyValues[V]) indexOf(key int) int {
+func (s *sliceKeyValues[K, V]) indexOf(key K) int {
 	for i, k := range s.keyValues {
 		if k.key == key {
 			return i
@@ -68,6 +68,6 @@ func (s *sliceKeyValues[V]) indexOf(key int) int {
 	return -1
 }
 
-func (s *sliceKeyValues[V]) String() string {
+func (s *sliceKeyValues[K, V]) String() string {
 	return fmt.Sprint("KeyValues", s.keyValues)
 }
--- interface.go
+++ interface.go
@@ -1,9 +1,15 @@
 package interfaces
 
-type Operations[T any] interface {
-	Has(key int) bool
-	Get(key int) *T
-	Remove(key int) *T
-	Set(key int, value T) *T
+import "golang.org/x/exp/constraints"
+
+type Operations[K, V CustomType] interface {
+	Has(key K) bool
+	Get(key K) *V
+	Remove(key K) *V
+	Set(key K, value V) *V
 	String() string
+}
+
+type CustomType interface {
+	constraints.Ordered
 }
